0b4b001fc6fbe9a846af489dd5f8cce3eb3bad7f
Jisang Y <jisangyou1@gmail.com>
2025-01-08 16:58:24 +0900

    added stl containers

--- src/TODO/stl_containers.cpp
+++ src/TODO/stl_containers.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <vector>
-#include <map>
 #include <algorithm>
 #include <deque>
 #include <queue>
 #include <stack>
 #include <set>
+#include <unordered_set>
+#include <map>
 
 /* This file demontrates STL containers
 - vector
 - deque
 - queue
 - stack
+- priority queue
 - map
 */
 
@@ -73,14 +75,51 @@ int main(){
     set.insert(10);
     set.insert(5);
     set.insert(20);
-
     for(int x : set){
         std::cout << x << " ";
     }
-
     set.erase(10); // returns 1 if elements exists, 0 if not
     std::cout << "\nExists: " << (set.count(10) ? "Yes" : "No") << std::endl; // Output
 
-    //Unordered Set
-    
+    // Unordered Set (Uses hashtable)
+    std::unordered_set<int> unordered_set;
+    unordered_set.insert(10);
+    unordered_set.insert(20);
+    unordered_set.insert(30);
+    // attempt to insert an element already in the set
+    unordered_set.insert(30); // this will be a nop
+    // Print all elements in the set
+    std::cout << "Elements in the unordered set: ";
+    for(const auto& x : unordered_set){
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+    // Check if 20 is in the set:
+    if(unordered_set.find(20) != unordered_set.end()){
+        std::cout << "20 is in the set" << std::endl;
+    }
+    unordered_set.erase(20);
+    std::cout << "The size of the unordered set is: " << unordered_set.size() << std::endl;
+
+    // Map
+    std::map<int, std::string> map;
+    // Insert elements
+    map[0] = "Apple";
+    map[1] = "Banana";
+    map[2] = "Cherry";
+    // Or use insert()
+    map.insert({4, "Date"});
+    // Print the key value pairs in the map
+    for(const auto& pair : map){
+        std::cout << "Key: " << pair.first << " Value: " << pair.second << std::endl;
+    }
+    // Check if a key exists in the map
+    if(map.find(2) != map.end()){
+        std::cout << "Key 3 found in the map" << std::endl;
+    }
+    // Erase key value pairs
+    map.erase(2);
+    std::cout << "The size of the map is: " << map.size() << std::endl;
+
+    // Unordered map - hashtable version of map. Same API, just no specific order.
 }
